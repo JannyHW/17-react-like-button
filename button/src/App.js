bbea8dac068bc0ba109e52b0ae8553d261a6d661
Jongdee H <jannykosin@gmail.com>
2020-09-28 22:28:22 -0700

    cannot run localhost:3000

---
+++ button/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import App from "./App.css";
+import Button from "./Button.js";
+
+function App() {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ button/src/Button.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+function Button() {
+  const [count, setCount] = useState(0);
+
+  let likes = () => (count !== 1) ? "Likes" : "Like";
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>
+        {count} {likes()}
+      </button>
+    </div>
+  );
+}
+
+export default Button;
